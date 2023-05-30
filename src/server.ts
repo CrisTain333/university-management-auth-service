0e3a2dfcc779d445fd09abb3a8c4f6f14066d3d1
Sukanta Das <arnobh84@gmail.com>
2023-05-30 11:24:24 +0600

    eslint and prettier setup completd

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import config from "./config";
-import app from "./app";
+import mongoose from 'mongoose';
+import config from './config';
+import app from './app';
 
 async function fire() {
-  try {
-    await mongoose.connect(config.database_url as string);
-    console.log("🛢️ Connected To Database");
-    app.listen(config.port, () => {
-      console.log(`Server Fire in http:localhost//${config.port}`);
-    });
-  } catch (error) {
-    console.log("Error to connect Database");
-  }
+   try {
+      await mongoose.connect(config.database_url as string);
+      console.log('🛢️ Connected To Database');
+      app.listen(config.port, () => {
+         console.log(`Server Fire in http:localhost//${config.port}`);
+      });
+   } catch (error) {
+      console.log('Error to connect Database');
+   }
 }
 fire();
