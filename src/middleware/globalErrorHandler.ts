aa544f6c8a53dabf3651c640a97de3b128f1d8c1
Sukanta Das <arnobh84@gmail.com>
2023-06-05 08:01:41 +0600

    global error hanlder function completed

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,25 +1,47 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 import handleValidationError from '../error/handleValidationError';
 import { IGenericErrorMessage } from '../interface/error';
 import config from '../config';
+import ApiError from '../error/ApiError';
 
-export const globalErrorHandler = (err, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     let statusCode = 500;
     let message = 'Something Went Wrong';
     let errorMessages: IGenericErrorMessage[] = [];
 
-    if (err?.name === 'ValidationError') {
-        const simpliFiedError = handleValidationError(err);
+    if (error?.name === 'ValidationError') {
+        const simpliFiedError = handleValidationError(error);
         statusCode = simpliFiedError?.statusCode;
         message = simpliFiedError?.message;
         errorMessages = simpliFiedError?.errorMessages;
+    } else if (error instanceof ApiError) {
+        statusCode = error?.statusCode;
+        message = error?.message;
+        errorMessages = error?.message
+            ? [
+                  {
+                      path: '',
+                      message: ''
+                  }
+              ]
+            : [];
+    } else if (error instanceof Error) {
+        message = error?.message;
+        errorMessages = error?.message
+            ? [
+                  {
+                      path: '',
+                      message: ''
+                  }
+              ]
+            : [];
     }
 
     res.status(statusCode).json({
         success: false,
         message,
         errorMessages,
-        stack: config.NODE_ENV !== 'production' ? err?.stack : undefined
+        stack: config.NODE_ENV !== 'production' ? error?.stack : undefined
     });
     next();
 };
