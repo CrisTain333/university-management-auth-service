2751ad75f75c5d08d1cb78c542659362832cb6ff
Sukanta Das <arnobh84@gmail.com>
2023-06-07 16:22:51 +0600

    zod schema installed

---
+++ src/app/modules/users/user.validate.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const userZodSchema = z.object({
+    body: z.object({
+        role: z.string({
+            required_error: 'role is required'
+        })
+    })
+});
+
+export const UserValidation = {
+    userZodSchema
+};
