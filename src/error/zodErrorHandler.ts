05714c202cf43e0ef3c276aa1e766f9ed0dcfbbe
Sukanta Das <arnobh84@gmail.com>
2023-06-08 14:22:50 +0600

    academic Interface created

--- src/error/zodErrorHandler.ts
+++ src/error/zodErrorHandler.ts
@@ -2,8 +2,6 @@ import { ZodError, ZodIssue } from 'zod';
 import { IGenericErrorMessage } from '../interface/error';
 
 const handleZodError = (error: ZodError) => {
-    console.log(error);
-
     const errors: IGenericErrorMessage[] = error?.issues?.map((issue: ZodIssue) => {
         return {
             path: issue?.path[issue?.path?.length - 1],
