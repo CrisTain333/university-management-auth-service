0286603746baf91d1439b24b60e69b3428edadca
Sukanta Das <arnobh84@gmail.com>
2023-06-08 19:27:11 +0600

    change in interface added months type

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -1,35 +1,24 @@
 import { Model } from 'mongoose';
+export type Month =
+    | 'January'
+    | 'February'
+    | 'March'
+    | 'April'
+    | 'May'
+    | 'June'
+    | 'July'
+    | 'August'
+    | 'September'
+    | 'October'
+    | 'November'
+    | 'December';
 
 export type IAcademicSemester = {
     title: 'Autumn' | 'Summer' | 'Fall';
     year: number;
     code: '01' | '02' | '03';
-    startMonth:
-        | 'January'
-        | 'February'
-        | 'March'
-        | 'April'
-        | 'May'
-        | 'June'
-        | 'July'
-        | 'August'
-        | 'September'
-        | 'October'
-        | 'November'
-        | 'December';
-    endMonth:
-        | 'January'
-        | 'February'
-        | 'March'
-        | 'April'
-        | 'May'
-        | 'June'
-        | 'July'
-        | 'August'
-        | 'September'
-        | 'October'
-        | 'November'
-        | 'December';
+    startMonth: Month;
+    endMonth: Month;
 };
 
 export type AcademicSemesterModel = Model<IAcademicSemester>;
