4f57ea6ab247f4d2499706fb6e92d62f897a1846
Sukanta Das <arnobh84@gmail.com>
2023-06-08 18:49:57 +0600

    academic Semester model updated with enum

--- src/app/modules/academicSemester/academicSemersterModel.ts
+++ src/app/modules/academicSemester/academicSemersterModel.ts
@@ -5,7 +5,8 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     {
         title: {
             type: String,
-            required: true
+            required: true,
+            enum: ['Autumn', 'Summer', 'Fall']
         },
         year: {
             type: Number,
@@ -13,15 +14,44 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
         },
         code: {
             type: String,
-            required: true
+            required: true,
+            enum: ['01', '02', '03']
         },
         startMonth: {
             type: String,
-            required: true
+            required: true,
+            enum: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ]
         },
         endMonth: {
             type: String,
-            required: true
+            required: true,
+            enum: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ]
         }
     },
     {
