58b8987a41a2e3cbe88457a23ac142545517f585
Sukanta Das <arnobh84@gmail.com>
2023-06-09 12:41:33 +0600

    send response function added in controllers

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -2,15 +2,19 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../../../shared/catchAsync';
 import { AcademicSemesterService } from './academicSemester.service';
+import sendResponse from '../../../shared/sendResponse';
 
 const createAcademicSemester = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
         const { ...academicSemesterData } = req.body;
         const result = await AcademicSemesterService.createAcademicSemester(academicSemesterData);
         next();
-        res.status(200).json({
+
+        // Send Response
+        sendResponse(res, {
+            statusCode: 200,
             success: true,
-            message: 'Academy semester created successfully!',
+            message: 'Academy semester created successfully',
             data: result
         });
     }
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
 
 const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
     const result = await UserService.createUser(user);
     next();
-    res.status(200).json({
+
+    // Send Response
+    sendResponse(res, {
+        statusCode: 200,
         success: true,
-        message: 'user created successfully!',
+        message: 'User Created Successfully',
         data: result
     });
 });
