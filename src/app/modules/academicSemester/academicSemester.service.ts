21a075eca2e3a5078c0582af753fab06b1ec6fb2
Sukanta Das <arnobh84@gmail.com>
2023-06-09 09:35:21 +0600

    semester code validation added in service leyear

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,13 @@
 import { AcademicSemester } from './academicSemesterModel';
 import { IAcademicSemester } from './academicSemester.interface';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
+import ApiError from '../../../error/ApiError';
 
 const createAcademicSemester = async (payload: IAcademicSemester): Promise<IAcademicSemester> => {
+    if (academicSemesterTitleCodeMapper[payload?.title] !== payload.code) {
+        throw new ApiError(400, 'Invalid Semester Code');
+    }
+
     const result = await AcademicSemester.create(payload);
 
     return result;
