a90aecc92b8b15e68305ea968deaa9f9ce784d72
Sukanta Das <arnobh84@gmail.com>
2023-06-09 08:34:08 +0600

    academic service added and change in academic modal name

---
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -0,0 +1,12 @@
+import { AcademicSemester } from './academicSemesterModel';
+import { IAcademicSemester } from './academicSemester.interface';
+
+const createSemester = async (payload: IAcademicSemester): Promise<IAcademicSemester> => {
+    const result = await AcademicSemester.create(payload);
+
+    return result;
+};
+
+export const AcademicSemesterService = {
+    createSemester
+};
