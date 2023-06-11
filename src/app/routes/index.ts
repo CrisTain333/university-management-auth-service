5f4515fb446b42f0dec69701e8dd365af0cdf8ab
Sukanta Das <arnobh84@gmail.com>
2023-06-11 22:24:23 +0600

    routes added for faculties and academic departments

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { UserRoute } from '../modules/users/user.route';
 import { SemesterRoute } from '../modules/academicSemester/academicSemester.routes';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academiFaculty.routes';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 
 const router = express.Router();
 
 router.use('/users', UserRoute);
 router.use('/academy-semesters', SemesterRoute);
+router.use('/academic-faculties', AcademicFacultyRoutes);
+router.use('/academic-departments', AcademicDepartmentRoutes);
 
 export default router;
