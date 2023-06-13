ba42566deb344eb16d369bef1c7b011eca0dcdf7
Sukanta Das <arnobh84@gmail.com>
2023-06-13 19:08:11 +0600

    student routes completed

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,24 +1,69 @@
-import { NextFunction, Request, Response } from 'express';
-// import { UserService } from './user.service';
+import { Request, Response } from 'express';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
-import { UserService } from '../users/user.service';
+import { paginationFields } from '../../../constant/pagination';
+import pick from '../../../shared/pick';
+import { studentFilterableFields } from './student.constant';
+import { IStudent } from './student.interface';
+import { StudentService } from './student.service';
 
-const getAllStudents = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const { student, ...userData } = req.body;
-    const result = await UserService.createStudent(student, userData);
+const getAllStudents = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, studentFilterableFields);
+    const paginationOptions = pick(req.query, paginationFields);
 
-    // Send Response
-    sendResponse(res, {
+    const result = await StudentService.getAllStudents(filters, paginationOptions);
+
+    sendResponse<IStudent[]>(res, {
         statusCode: 200,
         success: true,
-        message: 'User Created Successfully',
+        message: 'Students retrieved successfully !',
+        meta: result.meta,
+        data: result.data
+    });
+});
+
+const getSingleStudent = catchAsync(async (req: Request, res: Response) => {
+    const id = req.params.id;
+
+    const result = await StudentService.getSingleStudent(id);
+
+    sendResponse<IStudent>(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Student retrieved successfully !',
         data: result
     });
+});
 
-    next();
+const updateStudent = catchAsync(async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+
+    const result = await StudentService.updateStudent(id, updatedData);
+
+    sendResponse<IStudent>(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Student updated successfully !',
+        data: result
+    });
+});
+const deleteStudent = catchAsync(async (req: Request, res: Response) => {
+    const id = req.params.id;
+
+    const result = await StudentService.deleteStudent(id);
+
+    sendResponse<IStudent>(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Student deleted successfully !',
+        data: result
+    });
 });
 
 export const StudentController = {
-    getAllStudents
+    getAllStudents,
+    getSingleStudent,
+    updateStudent,
+    deleteStudent
 };
