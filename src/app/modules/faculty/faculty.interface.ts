984bd55d725ae930fbe81d3015691947e851ccfb
Sukanta Das <arnobh84@gmail.com>
2023-06-15 11:24:18 +0600

    facult interface created

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,26 @@
+import { Types } from 'mongoose';
+import { IAcademicDepartment } from '../academicDepartment/academicDepartment.interfaces';
+import { IAcademicFaculty } from '../academicFaculty/academicFaculty.interface';
+
+export type UserName = {
+    firstName: string;
+    lastName: string;
+    middleName: string;
+};
+
+export type IFaculty = {
+    id: string;
+    name: UserName;
+    dateOfBirth: string;
+    email: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    gender: 'male' | 'female';
+    permanentAddress: string;
+    presentAddress: string;
+    bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+    designation: string;
+    academicDepartment: Types.ObjectId | IAcademicDepartment;
+    academicFaculty: Types.ObjectId | IAcademicFaculty;
+    profileImage?: string;
+};
