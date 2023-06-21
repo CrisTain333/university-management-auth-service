2adc314e32d8edc07a079b132815978d6296f680
Sukanta Das <arnobh84@gmail.com>
2023-06-21 15:11:19 +0600

    routes updated

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,8 @@ import { SemesterRoute } from '../modules/academicSemester/academicSemester.rout
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academiFaculty.routes';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { StudentRoutes } from '../modules/student/student.routes';
+import { FacultyRoutes } from '../modules/faculty/faculty.routes';
+import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.routes';
 
 const router = express.Router();
 
@@ -12,5 +14,7 @@ router.use('/academy-semesters', SemesterRoute);
 router.use('/academic-faculties', AcademicFacultyRoutes);
 router.use('/academic-departments', AcademicDepartmentRoutes);
 router.use('/students', StudentRoutes);
+router.use('/faculties', FacultyRoutes);
+router.use('/management-departments', ManagementDepartmentRoutes);
 
 export default router;
