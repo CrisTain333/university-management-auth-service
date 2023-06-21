cc6cd50fc5053f17b44caab5eec67282ab9dcc46
Sukanta Das <arnobh84@gmail.com>
2023-06-21 14:37:15 +0600

    faculty model routes and validation  added

--- src/app/modules/faculty/faculty.model.ts
+++ src/app/modules/faculty/faculty.model.ts
@@ -1,12 +1,12 @@
-import { Schema, model } from 'mongoose';
-import { IFaculty, facultyModel } from './faculty.interface';
-import { bloodGroup, gender } from '../student/student.constant';
+import { Schema, Types, model } from 'mongoose';
+import { FacultyModel, IFaculty } from './faculty.interface';
 
-const facultySchema = new Schema<IFaculty>(
+const FacultySchema = new Schema<IFaculty, FacultyModel>(
     {
         id: {
             type: String,
-            required: true
+            required: true,
+            unique: true
         },
         name: {
             type: {
@@ -26,18 +26,27 @@ const facultySchema = new Schema<IFaculty>(
             required: true
         },
         dateOfBirth: {
-            type: String,
-            required: true
+            type: String
         },
         gender: {
             type: String,
-            enum: gender
+            enum: ['male', 'female']
+        },
+        bloodGroup: {
+            type: String,
+            enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
         },
         email: {
             type: String,
+            unique: true,
             required: true
         },
         contactNo: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        emergencyContactNo: {
             type: String,
             required: true
         },
@@ -49,38 +58,28 @@ const facultySchema = new Schema<IFaculty>(
             type: String,
             required: true
         },
-        emergencyContactNo: {
-            type: String,
+        academicDepartment: {
+            type: Types.ObjectId,
+            ref: 'AcademicDepartment',
             required: true
         },
         designation: {
             type: String,
             required: true
         },
-        bloodGroup: {
-            type: String,
-            enum: bloodGroup
-        },
         profileImage: {
             type: String
-        },
-        academicDepartment: {
-            type: Schema.Types.ObjectId,
-            ref: 'AcademicDepartment',
-            required: true
+            // required: true,
         },
         academicFaculty: {
-            type: Schema.Types.ObjectId,
+            type: Types.ObjectId,
             ref: 'AcademicFaculty',
             required: true
         }
     },
     {
-        timestamps: true,
-        toJSON: {
-            virtuals: true
-        }
+        timestamps: true
     }
 );
 
-export const Faculty = model<IFaculty, facultyModel>('Faculty', facultySchema);
+export const Faculty = model<IFaculty, FacultyModel>('Faculty', FacultySchema);
