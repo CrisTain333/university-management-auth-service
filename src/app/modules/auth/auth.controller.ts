38dac4f22a39723cb468af46427924958e2402ba
Sukanta Das <arnobh84@gmail.com>
2023-06-22 14:25:41 +0600

    cookie set to the browser

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,16 +2,27 @@ import { Request, Response } from 'express';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AuthService } from './auth.service';
+import config from '../../../config';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
     const { ...loginData } = req.body;
     const result = await AuthService.login(loginData);
+    const { refreshToken, ...others } = result;
+
+    // set refresh token into cookie
+
+    const cookieOptions = {
+        secure: config.NODE_ENV === 'production',
+        httpOnly: true
+    };
+
+    res.cookie('refreshToken', refreshToken, cookieOptions);
 
     sendResponse(res, {
         statusCode: 200,
         success: true,
         message: 'user login in successfully',
-        data: result
+        data: others
     });
 });
 
