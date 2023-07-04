a9989e330eff7b41af2143722d78ca245ecc6c82
Sukanta Das <arnobh84@gmail.com>
2023-07-04 09:01:46 +0600

    change password api completed

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import validateRequest from '../../../middleware/validateRequest';
 import { AuthValidation } from './auth.validation';
 import { AuthController } from './auth.controller';
+import auth from '../../../middleware/auth';
+import { USER_ENUM_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 router.post(
@@ -13,6 +15,17 @@ router.post(
     '/refresh-token',
     validateRequest(AuthValidation.refreshTokenZodSchema),
     AuthController.refreshToken
+);
+router.post(
+    '/change-password',
+    validateRequest(AuthValidation.changePasswordZodSchema),
+    auth(
+        USER_ENUM_ROLE.SUPER_ADMIN,
+        USER_ENUM_ROLE.ADMIN,
+        USER_ENUM_ROLE.FACULTY,
+        USER_ENUM_ROLE.STUDENT
+    ),
+    AuthController.changePassword
 );
 
 export const AuthRoutes = router;
